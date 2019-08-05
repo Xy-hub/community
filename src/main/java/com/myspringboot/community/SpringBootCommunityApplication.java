5959a7dd81abc0b73763b15f6a5437d90fbb8464
Xy-hub <2447315136@qq.com>
2019-08-05 16:05:50 +0800

    init repo

---
+++ src/main/java/com/myspringboot/community/SpringBootCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.myspringboot.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCommunityApplication.class, args);
+    }
+
+}
