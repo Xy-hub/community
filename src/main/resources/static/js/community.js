c81bdd2a8a2062515b92a21d80eb7679bacb839f
Xy_hub <2447315136@qq.com>
2019-08-20 18:05:19 +0800

    页面提交回复，未登录回复问题时进行一键登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -15,7 +15,16 @@ function post(){
            if(result.code==200){
                $("#comment_section").hide();
            }else{
-               alert(result.message);
+               if(result.code==2003){
+                    var confirm=window.confirm(result.message);
+                    if(confirm){
+                        window.open("https://github.com/login/oauth/authorize?client_id=d9716f23fa8342a094f3&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable","true");
+                    }
+               }else{
+                   alert(result.message);
+               }
+
            }
 
         }
